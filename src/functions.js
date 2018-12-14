4434c7343858a8cb22fa5f17d6ad350d794dd3da
Nawawish Samerpark <hydrokidz@gmail.com>
2018-12-14 15:57:45 +0700

    updated: getChatMessageList function

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,12 @@
 import ChatList from "./Chat/List";
 import ChatRoom from "./Chat/Room";
 import ChatAdmin from "./Chat/Admin";
+import ChatMessageList from "./Chat/MessageList";
 
 const getChatList = () => new ChatList("#_chat_list_body");
-
+const getChatMessageList = () => new ChatMessageList("#_chat_room_msg_list");
 const getChatRoom = () => new ChatRoom("#rightSide");
-
 const getChatAdmin = () =>
   new ChatAdmin({ list: getChatList(), room: getChatRoom() });
 
-export { getChatList, getChatRoom, getChatAdmin };
+export { getChatList, getChatRoom, getChatAdmin, getChatMessageList };
